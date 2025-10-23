4b23e5ed37269f4bcb838d6c0fd8822b315b11bc
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-10-23 15:12:53 +0000

    feat: function that adds two integers and returns the result of it

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Additionne deux entiers
+ * @a: Le premier entier à additionner
+ * @b: Le deuxième entier à additionner
+ *
+ * Return: La somme des deux entiers
+ */
+int add(int a, int b)
+{
+int addition = (a + b);
+return (addition);
+}
