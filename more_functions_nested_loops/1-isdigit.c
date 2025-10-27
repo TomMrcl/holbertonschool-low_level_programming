6dad9c83c0f2cbbe7c782c4942d5d5a9b079ba46
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-10-27 09:28:24 +0000

    fix: source main.h

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - Check for a digit (0-9).
+ * _isdigit - Check for a digit (0-9).
  * @c: The character code to check.
  *
  * Return: 1 if c is a digit, 0 otherwise.
