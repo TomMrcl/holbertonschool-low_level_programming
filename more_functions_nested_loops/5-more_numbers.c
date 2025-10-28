ebf288936937d059f0a755770640342682d13f01
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-10-28 08:52:32 +0000

    fix: correct output and betty style

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints 10 times the numbers, from 0 to 14, followed by a new line
+ * more_numbers - Affiche 10 fois les nombres de 0 à 14
  *
- * Return: nothing
+ * Return: Rien
  */
 
 void more_numbers(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
+		int j;
+
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar((j % 10) + '0');
+			if (j >= 10)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}
