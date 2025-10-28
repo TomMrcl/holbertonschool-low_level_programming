b65b7c220812dfad33121b863ebbc3bb2ef72ffe
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-10-28 10:08:41 +0000

    feat: ajouter la fonction print_diagonal pour dessiner une diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "../functions_nested_loops/main.h"
+/**
+ * print_diagonal - Déssine une diagonale
+ *
+ * @n: Nombre de diagonal
+ *
+ * Return: Rien
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		int j;
+
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
