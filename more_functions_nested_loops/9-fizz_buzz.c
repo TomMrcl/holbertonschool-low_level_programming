bef80fc45ff12258e1037c36cb7aa3c58a764c92
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-10-28 13:26:12 +0000

    feat: ajouter la fonction principale pour afficher les nombres de 1 à 100 avec FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - affiche les nombres de 1 à 100 en remplaçant :
+ *        multiples de 3 par "Fizz", de 5 par "Buzz" et de 15 par "FizzBuzz"
+ *
+ * Return: Retourne 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
