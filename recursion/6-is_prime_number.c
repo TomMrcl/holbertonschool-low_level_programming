1838451c9dc95cbc24fdb9dfc82783564a5becce
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-11-05 14:22:56 +0000

    feat: function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_helper - helps to determine if a number is prime
+ *
+ * @n: number to be checked
+ * @guess: number to divide n by
+ *
+ * Return: 1 if n is divisible by guess, 0 otherwise
+ */
+
+int is_prime_helper(int n, int guess)
+{
+	if (guess * guess > n)
+		return (1);
+	if (n % guess == 0)
+		return (0);
+	return (is_prime_helper(n, guess + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime_helper(n, 2));
+}
