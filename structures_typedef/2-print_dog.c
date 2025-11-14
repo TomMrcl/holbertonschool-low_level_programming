f52079db788236a31d202266d5f4013357b72c7d
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-11-14 13:51:46 +0000

    fix: corrige l'affichage des informations d'un chien dans print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,19 +12,33 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf("Dog is NULL\n");
 		return;
 	}
 
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	else
+	if (d->name == NULL)
+	{
 		printf("Name: (nil)\n");
-
-	printf("Age: %.6f\n", d->age);
-
-	if (d->owner != NULL)
-		printf("Owner: %s\n", d->owner);
+	}
 	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	if (d->age < 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+	{
 		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
