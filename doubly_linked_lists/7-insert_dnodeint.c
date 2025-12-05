a3dfb1723be9b78dc5e1b1fc391229ed75a9811b
Tom Marchal <168561077+TomMrcl@users.noreply.github.com>
2025-12-05 13:16:03 +0000

    feat: ajout de la fonction insert_dnodeint_at_index pour insérer un nœud à un index donné dans une liste doublement chaînée

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * @h: pointer to the head of the list
+ * @idx: index of the list where the new node should be added
+ * @n: data to be stored in the new node
+ *
+ * Return: pointer to the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+	dlistint_t *new_node, *current;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	current = *h;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	new_node->prev = current;
+	return (new_node);
+}
